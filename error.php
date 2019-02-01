722ff5efa11ce73435b15051a8fc5fc786bf5365
MazeOfEncryption <inbox@encryptedmaze.net>
2019-02-01 17:21:27 -0500

    spelling error

--- error.php
+++ error.php
@@ -2,7 +2,7 @@
 	<div class = 'jumbotron'>
 		<h2>Error.</h2>
 		<div class = 'alert alert-danger'>
-			An unexpected error occured. Please try again later.
+			An unexpected error occurred. Please try again later.
 		</div>
 		<a href = '/' class = 'btn btn-primary text-light'>Return to homepage</a>
 	</div>
